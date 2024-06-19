f4491ddb15bc7a6620fbb97d0377b892173e205b
iuliabadeaa <mariaiulia.badea@ulbsibiu.ro>
2024-06-19 20:39:08 +0300

    first commit not going so well

--- Lab1/patrat.cpp
+++ Lab1/patrat.cpp
@@ -1,168 +1,188 @@
 ﻿#include "glos.h"
-
 #include "gl.h"
 #include "glu.h"
 #include "glaux.h"
+#include "glut.h"
 
+GLfloat ctrlpoints[4][3] = {
+	{ -4.0, -4.0, 0.0}, { -2.0, 4.0, 0.0}, {2.0, 4.0, 0.0}, {4.0, -4.0, 0.0} };
+
+GLUquadricObj* quadric = gluNewQuadric();
+GLUquadricObj* circle = gluNewQuadric();
 static GLfloat x = 0;
 static GLfloat y = 0;
 static GLfloat alfa = 0;
+static GLfloat angle = 0;
 
+GLuint textureId1;
+GLuint textureId2;
 
-void myInit() {
-    glClearColor(1.0, 1.0, 1.0, 1.0);
-}
-
-void CALLBACK MutaStanga()
+GLuint incarcaTextura(const char* s)
 {
-    x = x - 10;
+	GLuint textureId = 0;
+	AUX_RGBImageRec* pImagineTextura = auxDIBImageLoad(s);
+
+	if (pImagineTextura != NULL)
+	{
+		glGenTextures(1, &textureId);
+		glBindTexture(GL_TEXTURE_2D, textureId);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+		glTexImage2D(GL_TEXTURE_2D, 0, 3, pImagineTextura->sizeX, pImagineTextura->sizeY,
+			0, GL_RGB, GL_UNSIGNED_BYTE, pImagineTextura->data);
+	}
+	if (pImagineTextura)
+	{
+		if (pImagineTextura->data) {
+			free(pImagineTextura->data);
+		}
+		free(pImagineTextura);
+	}
+	return textureId;
 }
 
-void CALLBACK MutaSus()
+void CALLBACK MutaStanga(void)
 {
-    y = y + 10;
+	x -= 1;
 }
 
-void CALLBACK MutaJos()
+void CALLBACK MutaDreapta(void)
 {
-    y = y - 10;
+	x += 1;
 }
 
-void CALLBACK MutaDreapta()
+void CALLBACK MutaSus(void)
 {
-    x = x + 10;
+	y += 1;
 }
 
-void CALLBACK rotate_z_up(AUX_EVENTREC* event)
+void CALLBACK MutaJos(void)
 {
-    alfa += 30;
+	y -= 1;
 }
-
-void CALLBACK rotate_z_down(AUX_EVENTREC* event)
+void CALLBACK rot_z_up(AUX_EVENTREC* event)
 {
-    alfa -= 30;
+	alfa += 2;
 }
 
-
-void drawOrigin() {
-    glPointSize(5.0);
-    glColor3f(1.0f, 0.0f, 0.0f);
-    glBegin(GL_POINTS);
-    glVertex3f(0.0f, 0.0f, 0.0f);
-    glEnd();
-}
-
-
-void CALLBACK display()
+void CALLBACK rot_z_down(AUX_EVENTREC* event)
 {
-    glClear(GL_COLOR_BUFFER_BIT);
-
-    glLoadIdentity();
-
-    glTranslatef(x, y, 0.0);
-    glRotatef(alfa, 0, 1, 0);
-
-
-    //dreapta
-    glBegin(GL_LINE_LOOP);
-    {
-        glColor3f(0.0, 0.0, 1.0);
-        glVertex3d(0.0, 100.0, 0.0);
-        glColor3f(0.0, 0.0, 1.0);
-        glVertex3d(60.0, 0.0, 60.0);
-        glColor3f(0.0, 0.0, 1.0);
-        glVertex3d(60.0, 0.0, -60.0);
-    }
-    glEnd();
-
-
-    //stanga
-    glBegin(GL_LINE_LOOP);
-    {
-        glColor3f(0.0, 0.0, 1.0);
-        glVertex3d(0.0, 100.0, 0.0);
-        glColor3f(0.0, 0.0, 1.0);
-        glVertex3d(-60.0, 0.0, 60.0);
-        glColor3f(0.0, 0.0, 1.0);
-        glVertex3d(-60.0, 0.0, -60.0);
-    }
-    glEnd();
-
-    //spate
-    glBegin(GL_LINE_LOOP);
-    {
-        glColor3f(0.0, 0.0, 1.0);
-        glVertex3d(0.0, 100.0, 0.0);
-        glColor3f(0.0, 0.0, 1.0);
-        glVertex3d(-60.0, 0.0, -60.0);
-        glColor3f(0.0, 0.0, 1.0);
-        glVertex3d(60.0, 0.0, -60.0);
-    }
-    glEnd();
-
-    //fata
-    glBegin(GL_LINE_LOOP);
-    {
-        glColor3f(0.0, 0.0, 1.0);
-        glVertex3d(0.0, 100.0, 0.0);
-        glColor3f(0.0, 0.0, 1.0);
-        glVertex3d(60.0, 0.0, 60.0);
-        glColor3f(0.0, 0.0, 1.0);
-        glVertex3d(-60.0, 0.0, 60.0);
-    }
-    glEnd();
-
-
-
-    drawOrigin();
-
-    glFlush();
+	alfa -= 2;
 }
 
-#define ISOTROPIC
-#ifdef ISOTROPIC
+void CALLBACK rot_y_left()
+{
+	angle += 2;
+}
+
+void CALLBACK rot_y_right()
+{
+	angle -= 2;
+}
+
+
+
+
+void myInit()
+{
+	glClearColor(1.0, 1.0, 1.0, 1.0);
+	glEnable(GL_MAP1_VERTEX_3);
+	textureId1 = incarcaTextura("covor.bmp");
+	glEnable(GL_LIGHTING);
+	glEnable(GL_LIGHT0);
+	glColorMaterial(GL_FRONT_AND_BACK, GL_DIFFUSE);
+	glEnable(GL_COLOR_MATERIAL);
+}
+
+
+void drawString(const char* str, int x, int y) {
+	glRasterPos2i(x, y);
+
+	for (const char* c = str; *c != '\0'; c++) {
+		glutBitmapCharacter(GLUT_BITMAP_HELVETICA_12, *c);
+	}
+}
+
+
+void CALLBACK display(void)
+{
+	glLoadIdentity();
+	glClear(GL_COLOR_BUFFER_BIT);
+	glColor3f(0.0f, 0.0f, 0.0f);
+	glTranslatef(x, y, 0.0f);
+	glRotatef(alfa, 1, 0, 0);
+	glRotatef(angle, 0, 1, 0);
+
+	glLineWidth(3.0f);
+
+	glMap1f(GL_MAP1_VERTEX_3, 0.0, 1.0, 3, 4, &ctrlpoints[0][0]);
+
+
+	glRotatef(90, 1, 0, 0);
+	glTranslatef(0, 0, 4.0);
+	glColor3f(1, 1, 0.2);
+	glEnable(GL_TEXTURE_2D);
+	gluQuadricTexture(quadric, GL_TRUE);
+	glBindTexture(GL_TEXTURE_2D, textureId1);
+	gluCylinder(quadric, 4.0f, 4.0f, 0.75f, 60, 6);
+	glColor3f(0.9, 0.8, 0.3);
+	gluQuadricTexture(circle, GL_TRUE);
+	gluDisk(circle, 0, 4.0f, 60, 6);
+	glRotatef(-90, 1, 0, 0);
+	glTranslatef(0, 4, 0);
+
+	for (int i = 10; i < 120; i += 10)
+	{
+		glRotatef(i, 0.0f, 1.0f, 0.0f);
+		glColor3f(1.0f, i / 120.0f, 0.0f);
+
+		glBegin(GL_LINE_STRIP);
+		{
+			glEnable(GL_AUTO_NORMAL);
+			for (int j = 0; j <= 50; j++) {
+				glEvalCoord1f((GLfloat)j / 50.0f);
+			}
+		}
+		glEnd();
+	}
+
+	glPushMatrix();
+
+	glColor3f(0.0, 0.0, 0.0);
+	drawString("Badea Maria-Iulia", 0, 7);
+
+	glPopMatrix();
+
+
+	glFlush();
+}
+
 void CALLBACK myReshape(GLsizei w, GLsizei h)
 {
-    if (!h) {
-        return;
-    }
-    glViewport(0, 0, w, h);
-    glMatrixMode(GL_PROJECTION);
-    glLoadIdentity();
-    if (w <= h) {
-        glOrtho(-160.0, 160.0, 160.0 * (GLfloat)h / (GLfloat)w, -160.0 * (GLfloat)h / (GLfloat)w, -10.0, 10.0);
-    }
-    else {
-        glOrtho(-160.0 * (GLfloat)w / (GLfloat)h, 160.0 * (GLfloat)w / (GLfloat)h, -160.0, 160.0, -10.0, 10.0);
-    }
-    glMatrixMode(GL_MODELVIEW);
+	if (!h) return;
+	glViewport(0, 0, w, h);
+	glMatrixMode(GL_PROJECTION);
+	glLoadIdentity();
+	gluPerspective(65.0f, w/h, -5, 20);
+	gluLookAt(0, 4, 24, 0, 4, 0, 0, 1, 0);
+	glMatrixMode(GL_MODELVIEW);
 }
-#else
-void CALLBACK myReshape(GLsizei w, GLsizei h)
-{
-    if (!h) return;
-    glViewport(0, 0, w, h);
-    glMatrixMode(GL_PROJECTION);
-    glLoadIdentity();
-    glOrtho(-160.0, 160.0, -160.0, 160.0, -10.0, 10.0);
-    glMatrixMode(GL_MODELVIEW);
-}
-#endif
 
 int main(int argc, char** argv)
 {
-    auxInitDisplayMode(AUX_SINGLE | AUX_RGB);
-    auxInitPosition(0, 0, 800, 600);
-    auxInitWindow("Un patrat");
-    myInit();
-    auxKeyFunc(AUX_LEFT, MutaStanga);
-    auxKeyFunc(AUX_RIGHT, MutaDreapta);
-    auxKeyFunc(AUX_UP, MutaSus);
-    auxKeyFunc(AUX_DOWN, MutaJos);
-    //auxKeyFunc(AUX_q, rotate_z_down);
-
-
-    auxReshapeFunc(myReshape);
-    auxMainLoop(display);
-    return(0);
+	auxInitDisplayMode(AUX_SINGLE | AUX_RGB);
+	auxInitPosition(0, 0, 500, 500);
+	auxInitWindow("Colivie RIP");
+	myInit();
+	auxReshapeFunc(myReshape);
+	auxKeyFunc(AUX_LEFT, MutaStanga);
+	auxKeyFunc(AUX_RIGHT, MutaDreapta);
+	auxKeyFunc(AUX_UP, MutaSus);
+	auxKeyFunc(AUX_DOWN, MutaJos);
+	auxMouseFunc(AUX_LEFTBUTTON, AUX_MOUSEDOWN, rot_z_up);
+	auxMouseFunc(AUX_RIGHTBUTTON, AUX_MOUSEUP, rot_z_down);
+	auxKeyFunc(AUX_w, rot_y_left);
+	auxKeyFunc(AUX_s, rot_y_right);
+	auxMainLoop(display);
+	return 0;
 }
